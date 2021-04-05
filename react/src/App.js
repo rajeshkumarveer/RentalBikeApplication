cbf192a87a7a726c5cba43dc1973cd0cd850151c
Rajesh <rajeshkumarveer2305@gmail.com>
2021-04-05 22:32:19 +0530

    add application

---
+++ react/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import routes from "./routes";
+import helperService from "./Services/helperService";
+
+function App() {
+  return (
+    <Router>
+      {helperService.checkLogin()}
+      {routes}
+    </Router>
+  );
+}
+
+export default App;
